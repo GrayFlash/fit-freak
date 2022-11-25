0d5ed78ec22e7eb073305ac69559921d808f6f70
GrayFlash <gk74533@gmail.com>
2022-11-26 02:39:10 +0530

    feat: add client and server for p2p video call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, useNavigate, Route } from "react-router-dom";
 import HomePage from "./Views/HomePage";
 import PoseAcc from "./Views/PoseAcc";
+import Room from './Views/Room';
 
 const App = () => {
+  // let navigate = useNavigate();
+
   return (
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/room/:roomID" element={<Room/>} />
           <Route path="/poseaccuracy" element={<PoseAcc />} />
         </Routes>
       </Router>
